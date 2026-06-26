66ff4bc7a6804f6d45b6ca48e3c83cfa334164f9
fineanmol <anmol.agarwal2004@yahoo.com>
2026-06-26 02:53:06 +0200

    seo: rename extension to Bulk Delete ChatGPT History & Chat Cleaner and set clickbait app icon

--- content.js
+++ content.js
@@ -993,7 +993,7 @@
                 <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
               </svg>
             </div>
-            <h1 class="cbd-brand-title">ChatGPT Bulk Delete Manager</h1>
+            <h1 class="cbd-brand-title">Bulk Delete ChatGPT History</h1>
           </div>
 
           <div class="cbd-header-actions-group">
